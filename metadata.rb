2a99b3ca844cbbda3c67502f36e021e7deab33bd
Nathan Mische <nmische@gmail.com>
2012-02-18 11:10:31 -0500

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "NATHAN MISCHE"
+maintainer_email "nmische@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures coldfusion10"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
