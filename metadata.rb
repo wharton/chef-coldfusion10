da942ebd7bb06814ec910765e5ee1eea4fbf34ef
Nathan Mische <nmische@gmail.com>
2012-11-13 14:33:02 -0500

    Fixing permissions when installing with runtime user. Bumping version number.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "nmische@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures Adobe ColdFusion 10"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.5"
+version          "0.0.6"
 
 %w{ ubuntu }.each do |os|
   supports os
