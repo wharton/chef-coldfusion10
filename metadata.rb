6ce51fe9fb3f039b4d54e3800632b6d067e813ea
Nathan Mische <nmische@gmail.com>
2013-02-20 11:12:32 -0500

    Adding CFFileServlet alias and removing trailing slashes from CFIDE aliases.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "nmische@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures Adobe ColdFusion 10"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.4"
+version          "0.1.5"
 
 %w{ redhat ubuntu }.each do |os|
   supports os
