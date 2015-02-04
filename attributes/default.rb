6e6200ddeca685a33171435115f4a29b2634f3a5
Daryl Banttari <daryl@invisionapp.com>
2015-02-04 09:32:40 -0600

    add hotfix 15

--- attributes/default.rb
+++ attributes/default.rb
@@ -116,6 +116,7 @@ default['cf10']['updates']['urls'] = %w{
   http://download.adobe.com/pub/adobe/coldfusion/hotfix_012.jar
   http://download.adobe.com/pub/adobe/coldfusion/hotfix_013.jar
   http://download.adobe.com/pub/adobe/coldfusion/hotfix_014.jar
+  http://download.adobe.com/pub/adobe/coldfusion/hotfix_015.jar
 }
 default['cf10']['updates']['files'] = %w{ 
   hf1000-3332326.jar
@@ -132,6 +133,7 @@ default['cf10']['updates']['files'] = %w{
   chf10000012.jar
   chf10000013.jar
   chf10000014.jar
+  chf10000015.jar
 }
 
 # Tomcat or Apache web root
