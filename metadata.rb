7466d46181d75a5e66d009a13ac29eb888b6de59
JonsJava <jonsjava@gmail.com>
2016-03-16 10:32:05 -0500

    Re-worked restart init script to try more graceful stops.  Runs through multiple methods to stop the service.  On fifth failure, it will notify you that it failed.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "nmische@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures Adobe ColdFusion 10"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.15"
+version          "0.4.16"
 
 %w{ centos redhat ubuntu }.each do |os|
   supports os
